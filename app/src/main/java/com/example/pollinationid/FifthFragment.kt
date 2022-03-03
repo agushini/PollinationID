4ff71b2a25ccc4a0e676bf98182a1c8ab65fb75d
bkacic <Brendan.kacic02@gmail.com>
2022-03-03 14:28:23 -0500

    Updating Google JSON to FB

--- app/src/main/java/com/example/pollinationid/FifthFragment.kt
+++ app/src/main/java/com/example/pollinationid/FifthFragment.kt
@@ -1,13 +1,10 @@
 package com.example.pollinationid
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import kotlinx.android.synthetic.main.fragment_fifth.*
-
 
 class FifthFragment:Fragment(R.layout.fragment_fifth) {
 
 
 }
-
