55e7ff3f9994647cbf19f0094c671af6de3ba593
bkacic <Brendan.kacic02@gmail.com>
2022-03-14 13:20:55 -0400

    Fixing Something with fragment error
    trying to fix the activity main and fragment error

--- app/src/main/java/com/example/pollinationid/MainActivity.kt
+++ app/src/main/java/com/example/pollinationid/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.pollinationid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -34,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //this needs to be edited, i was trying to figure out how to change the action bar
-    private fun setCurrentFragment(fragment: FifthFragment)=
+    private fun setCurrentFragment(fragment:Fragment)=
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.flFragment,fragment)
            /* if (fragment == FirstFragment()){
