2ea3cc6e7a2ae4fb84d689bc1f00509c8638ec86
Cam <58830545+camie730@users.noreply.github.com>
2022-03-16 14:32:19 -0400

    Created next page in Dichotomous Key

--- app/src/main/java/com/example/pollinationid/SecondFragment.kt
+++ app/src/main/java/com/example/pollinationid/SecondFragment.kt
@@ -1,10 +1,21 @@
 package com.example.pollinationid
-
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment:Fragment(R.layout.fragment_second) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        idButton.setOnClickListener {
+//            //val intent = Intent(idButton.context, PollinatorIDKeyActivity::class.java)
+//            //startActivity(intent)
+//            //idButton.context.startActivity(intent)
+//            Log.v("Second Fragment: ","NExt button clicked")
+//        }
+
+    }
 }
