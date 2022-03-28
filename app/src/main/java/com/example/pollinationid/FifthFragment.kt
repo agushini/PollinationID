f42b8a35b499983999cf4703582a566807afb02c
Christopher Bourn <cbourn@umich.edu>
2022-03-28 16:57:28 -0400

    Made menu buttons

--- app/src/main/java/com/example/pollinationid/FifthFragment.kt
+++ app/src/main/java/com/example/pollinationid/FifthFragment.kt
@@ -14,36 +14,6 @@ class FifthFragment:Fragment(R.layout.fragment_fifth) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val listView = view?.findViewById<ListView>(R.id.main_listview)
-
-        listView?.adapter = MyCustomAdapter(this.requireContext())//SUPPOSED TO JUST BE "this" BUT IT GIVES ERRORS
-    }
-
-    private class MyCustomAdapter(context: Context): BaseAdapter() {
-
-        private val mContext: Context = context
-
-        //responsible for how many rows in my list
-        override fun getCount(): Int {
-            return 5
-        }
-
-        //Ignore this for now
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        //Ignore this for now
-        override fun getItem(position: Int): Any {
-            return "getItem <TEST STRING>"
-        }
-
-        //responsible for rendering out each row
-        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
-            val textView = TextView(mContext)
-            textView.text = "Here is my ROW for my LISTVIEW"
-            return textView
-        }
 
     }
 
