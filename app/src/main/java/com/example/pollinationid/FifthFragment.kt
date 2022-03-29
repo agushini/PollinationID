390add518309449062549ff37a85fc38d0b08c72
Christopher Bourn <cbourn@umich.edu>
2022-03-29 15:00:17 -0400

    -_-
    MY COMPUTER IS RUNNING AT -1 FPS I'M GONNA GO CRAZY

--- app/src/main/java/com/example/pollinationid/FifthFragment.kt
+++ app/src/main/java/com/example/pollinationid/FifthFragment.kt
@@ -1,20 +1,25 @@
 package com.example.pollinationid
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
+import android.util.Log
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import android.widget.ListView
-import android.widget.TextView
+
 
 class FifthFragment:Fragment(R.layout.fragment_fifth) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val signOut = view?.findViewById<Button>(R.id.btnSignOut)
+
+        signOut?.setOnClickListener(){
+            Log.i("Good", "SignOut Button Clicked")
+            val intent = Intent (activity, LoginActivity::class.java)
+            activity?.startActivity(intent)
+        }
 
     }
-
 }
