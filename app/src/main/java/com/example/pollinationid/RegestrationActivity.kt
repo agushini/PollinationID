9fd40fc3fef9cf30a11d186e601e1bf63624949f
agushini <56412460+agushini@users.noreply.github.com>
2022-04-12 10:50:51 -0400

    Database connection
    working on the registration fixing for the sign up because i need to be able to access the stuff idk

--- app/src/main/java/com/example/pollinationid/RegestrationActivity.kt
+++ app/src/main/java/com/example/pollinationid/RegestrationActivity.kt
@@ -99,11 +99,12 @@ class RegestrationActivity : AppCompatActivity() {
         }
 
         val user = hashMapOf(
-            "first_name" to "Ada",
-            "last_name" to "Lovelace",
+            "first_name" to FirstName,
+            "last_name" to LastName,
+            "admin" to false,
         )
 // Add a new document with a generated ID
-        db.collection("users")
+        db.collection("Users")
             .add(user)
             .addOnSuccessListener { documentReference ->
                 Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
@@ -111,16 +112,16 @@ class RegestrationActivity : AppCompatActivity() {
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
             }
-        db.collection("users")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d(TAG, "${document.id} => ${document.data}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents.", exception)
-            }
+//        db.collection("Users") //Sukun i commented this out to try to fix this, if it works im getting rid of it
+//            .get()
+//            .addOnSuccessListener { result ->
+//                for (document in result) {
+//                    Log.d(TAG, "${document.id} => ${document.data}")
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.w(TAG, "Error getting documents.", exception)
+//            }
 
         /*val user = Firebase.auth.currentUser
         user?.let {
