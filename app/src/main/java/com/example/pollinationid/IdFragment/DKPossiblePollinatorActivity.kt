38b7445223fe5e9adea3c8dc247add393358adcf
Cam <58830545+camie730@users.noreply.github.com>
2022-04-13 23:51:09 -0400

    changes all pages to activities and added all button clicks

---
+++ app/src/main/java/com/example/pollinationid/IdFragment/DKPossiblePollinatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pollinationid.IdFragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pollinationid.R
+
+class DKPossiblePollinatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dkpossible_pollinator)
+    }
+}
--- app/src/main/java/com/example/pollinationid/IdFragment/HairActivity.kt
+++ app/src/main/java/com/example/pollinationid/IdFragment/HairActivity.kt
@@ -11,10 +11,30 @@ class HairActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hair)
 
-        //different Wings button click to hairs page
+        //almost no hair click to almost no hair color page
         val almostNoHairBttn = findViewById<ImageButton>(R.id.noHairButton)
         almostNoHairBttn.setOnClickListener {
             val intent = Intent(this, AlmostNoHairColorActivity::class.java)
+            startActivity(intent)
+        }
+
+        //hair underneath click to hair underneath color page
+        val hairUnderneathBttn = findViewById<ImageButton>(R.id.hairUnderneathButton)
+        hairUnderneathBttn.setOnClickListener {
+            val intent = Intent(this, HairUnderneathColorActivity::class.java)
+            startActivity(intent)
+        }
+
+        //hair on sides click to hair on sides color page
+        val hairOnSidesBttn = findViewById<ImageButton>(R.id.hairOnSideButton)
+        hairOnSidesBttn.setOnClickListener {
+            val intent = Intent(this, HairOnSidesColorActivity::class.java)
+            startActivity(intent)
+        }
+
+        val fullHairBttn = findViewById<ImageButton>(R.id.hairFullButton)
+        fullHairBttn.setOnClickListener {
+            val intent = Intent(this, FullHairColorActivity::class.java)
             startActivity(intent)
         }
 
